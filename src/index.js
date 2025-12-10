7558e65cf2fdd5f5ba93dfc4473c9cb00e347be3
JacksonGreninger <jwg019@bucknell.edu>
2025-12-10 16:23:22 -0500

    tryin to fix white screen

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app/App";
 import "./index.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
